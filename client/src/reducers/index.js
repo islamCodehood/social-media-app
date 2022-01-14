2621dc7a9a52e007669a09e65e538c98e0dc4691
Islam Sayed <islam.sayed8@gmail.com>
2022-01-14 09:56:56 +0200

    create redux reducers

---
+++ client/src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux'
+import posts from './posts.reducer'
+
+export const reducers = combineReducers({ posts });
---
+++ client/src/reducers/posts.reducer.js
@@ -0,0 +1,14 @@
+const reducer =  (state = [], action) => {
+    switch (action.type) {
+        case `FETCH_ALL`:
+            return action.payload;
+        case 'CREATE':
+            return [...state, action.payload];
+        default:
+            return state;
+    }
+}
+
+export default reducer
+
+
