e542d2a90a610a0d8b954dfe98dee32cd8cfe2bf
Islam Sayed <islam.sayed8@gmail.com>
2022-01-14 09:53:37 +0200

    create posts requests controllers

---
+++ server/controllers/posts.controllers.js
@@ -0,0 +1,20 @@
+import Post from '../models/post.model.js'
+
+export const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.status(200).json(posts)
+    } catch (err) {
+        res.status(404).json({ message: err.message })
+    }
+}
+export const createPost = async (req, res) => {
+    const post = req.body
+    const newPost = new Post(post)
+    try {
+        await newPost.save()
+        res.status(201).json(newPost)
+    } catch (err) {
+        res.status(409).json({ message: err.message })
+    }
+}
