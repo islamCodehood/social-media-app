c5c8123d1e80d3aa85d5cabf6e60a899b49e69f7
Islam Sayed <islam.sayed8@gmail.com>
2022-01-14 09:55:19 +0200

    create posts redux actions

---
+++ client/src/actions/posts.actions.js
@@ -0,0 +1,21 @@
+import * as api from "../api";
+
+//Action Creators
+
+export const getPosts = () => async (dispatch) => {
+    try {
+        const { data } = await api.fetchPosts();
+		dispatch({ type: "FETCH_ALL", payload: data });
+	} catch (err) {
+		console.log(err.message);
+	}
+};
+
+export const createPost = (newPost) => async (dispatch) => {
+	try {
+		const { data } = await api.createPost(newPost)
+		dispatch({ type: "CREATE", payload: data });
+	} catch (err) {
+		console.log(err.message);
+	}
+}
