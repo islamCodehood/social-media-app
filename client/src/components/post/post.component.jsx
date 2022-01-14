676a932f8505e053d68c6d2f9f4abd105e147516
Islam Sayed <islam.sayed8@gmail.com>
2022-01-14 09:56:19 +0200

    create post comp

---
+++ client/src/components/post/post.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import useStyles from "./post.styles.js"
+const Post = () => {
+    const classes = useStyles()
+    return (
+        <h1>post</h1>
+    )   
+}
+
+export default Post;
