a9e0e33b75ed778b28919be11ed912dc4263a083
Islam Sayed <islam.sayed8@gmail.com>
2022-01-16 11:13:02 +0200

    create comp

--- client/src/components/post/post.styles.js
+++ client/src/components/post/post.styles.js
@@ -40,6 +40,7 @@ export default makeStyles({
 		display: "flex",
 		justifyContent: "space-between",
 		margin: "20px",
+		marginTop: "10px"
 	},
 	title: {
 		padding: "0 16px",
--- client/src/components/posts/posts.component.jsx
+++ client/src/components/posts/posts.component.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { Grid, CircularProgress } from '@material-ui/core'
 import Post from '../post/post.component';
 import useStyles from './posts.styles.js';
 import { useSelector } from 'react-redux' 
-const Posts = () => {
+
+const Posts = ({ setCurrentId }) => {
     const classes = useStyles()
+	/**** */
 	const posts = useSelector((state) => state.posts)
 	console.log(posts)
-	return (
-		<>
-			<h1>Posts</h1>
-			<Post />
-			<Post />
-		</>
+	/**** */
+	return (	
+		
+		!posts.length ? <CircularProgress color="primary" /> : (
+			<Grid container className={classes.container} alignItems="stretch" spacing={3}>
+				{
+					posts.map(post => 
+							<Grid item key={post._id} xs={12} sm={6}>
+								<Post post={post} setCurrentId={setCurrentId} />
+							</Grid>
+						)
+				}
+			</Grid>
+		)
 	);
 };
 
