648774e9df4bf2c2140af95b1a0deb402976baf3
Islam Sayed <islam.sayed8@gmail.com>
2022-01-16 11:15:30 +0200

    create updatePost controller

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -1,20 +1,34 @@
-import Post from '../models/post.model.js'
+import Post from "../models/post.model.js";
+import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
-    try {
-        const posts = await Post.find()
-        res.status(200).json(posts)
-    } catch (err) {
-        res.status(404).json({ message: err.message })
-    }
-}
+	try {
+		const posts = await Post.find();
+		res.status(200).json(posts);
+	} catch (err) {
+		res.status(404).json({ message: err.message });
+	}
+};
+
 export const createPost = async (req, res) => {
-    const post = req.body
-    const newPost = new Post(post)
-    try {
-        await newPost.save()
-        res.status(201).json(newPost)
-    } catch (err) {
-        res.status(409).json({ message: err.message })
-    }
-}
+	const post = req.body;
+	const newPost = new Post(post);
+	try {
+		await newPost.save();
+		res.status(201).json(newPost);
+	} catch (err) {
+		res.status(409).json({ message: err.message });
+	}
+};
+
+export const updatePost = async (req, res) => {
+	console.log(req.params);
+    //get the post id
+	const { id: _id } = req.params;
+	const post = req.body;
+    //check if the id is a valid id that is a valid Id
+	//if (!mongoose.Types.objectId.isValid(_id)) return res.status(404).send("No post with that id");
+    //Get the post and update it
+	const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true });
+    res.status(200).json(updatedPost);
+};
