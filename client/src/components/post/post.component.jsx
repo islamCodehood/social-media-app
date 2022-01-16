5f43609062b9a540e433725903e9d40574559258
Islam Sayed <islam.sayed8@gmail.com>
2022-01-16 11:12:37 +0200

    create comp

--- client/src/components/post/post.component.jsx
+++ client/src/components/post/post.component.jsx
@@ -1,10 +1,50 @@
 import React from "react";
-import useStyles from "./post.styles.js"
-const Post = () => {
-    const classes = useStyles()
-    return (
-        <h1>post</h1>
-    )   
-}
+import { Card, CardContent, CardActions, CardMedia, Typography, Button } from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment"
+import useStyles from "./post.styles.js";
+
+const Post = ({ post, setCurrentId }) => {
+	const classes = useStyles();
+    moment.relativeTimeThreshold('d', 25);
+	return (
+		<Card className={classes.card}>
+			<CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+			<div className={classes.overlay}>
+				<Typography variant="h6">{post.creator}</Typography>
+				<Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+			</div>
+			<div className={classes.overlay2}>
+				<Button style={{ color: "white" }} size="small" onClick={() => {setCurrentId(post._id)}}>
+					<MoreHorizIcon fontSize="medium" />
+				</Button>
+			</div>
+			<div className={classes.details}>
+				<Typography variant="body2" color="textSecondary">
+
+					{post.tags.map((tag, index) => <span key={index}>{`#${tag} `}</span>)}
+				</Typography>
+			</div>
+			<CardContent>
+				<Typography className={classes.title} variant="body1" gutterBottom>
+					{post.message}
+				</Typography>
+			</CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    <ThumbUpAltIcon fontSize="small" />
+                    Like
+                    {post.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={() => {}}>
+                    <DeleteIcon fontSize="small" />
+                    Delete   
+                </Button>
+            </CardActions>
+		</Card>
+	);
+};
 
 export default Post;
