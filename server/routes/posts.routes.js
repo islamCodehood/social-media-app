92f34f62430262c7dcd72b854c1b3bb55c7b911d
Islam Sayed <islam.sayed8@gmail.com>
2022-01-16 11:15:54 +0200

    add PATCH route

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getPosts, createPost } from "../controllers/posts.controllers.js";
+import { getPosts, createPost, updatePost } from "../controllers/posts.controllers.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost);
 
 export default router;
