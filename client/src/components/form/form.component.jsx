6da6e66957d14596aef42178d543911ff9011121
Islam Sayed <islam.sayed8@gmail.com>
2022-01-18 11:57:08 +0200

    add functionality to clear form afetr submitting

--- client/src/components/form/form.component.jsx
+++ client/src/components/form/form.component.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { createPost, updatePost } from "../../actions/posts.actions";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from "react-file-base64";
-
 import useStyles from "./form.styles.js";
+
 const Form = ({ currentId, setCurrentId }) => {
 	const [postData, setPostData] = useState({
 		creator: "",
@@ -14,6 +14,12 @@ const Form = ({ currentId, setCurrentId }) => {
 		selectedFile: "",
 	});
 
+	const post = useSelector((state) => currentId ? state.posts.find(p => p._id === currentId) : null)
+	useEffect(() => {
+		if (post) {
+			setPostData(post);
+		}
+	}, [post])
 	const classes = useStyles();
 	const dispatch = useDispatch();
 
@@ -25,12 +31,23 @@ const Form = ({ currentId, setCurrentId }) => {
           } else {
                dispatch(createPost(postData));
           }
+		  clear()
 	};
-	const clear = () => {};
+	
+	const clear = () => {
+		setCurrentId(null)
+		setPostData({
+			creator: "",
+			title: "",
+			message: "",
+			tags: [],
+			selectedFile: "",
+		});
+	};
 	return (
 		<Paper className={classes.paper}>
 			<form className={`${classes.root} ${classes.form}`} autoComplete="off" noValidate onSubmit={handleSubmit}>
-				<Typography variant="h6">Creating a memory</Typography>
+				<Typography variant="h6">{currentId ? 'Editing' : 'Creating'} a memory</Typography>
 				<TextField
 					name="creator"
 					variant="outlined"
@@ -60,7 +77,7 @@ const Form = ({ currentId, setCurrentId }) => {
 					variant="outlined"
 					label="Tags"
 					fullWidth
-					/* value={postData.tags}  */
+					value={postData.tags}
 					onChange={(e) => setPostData({ ...postData, tags: e.target.value.replaceAll(" ", "").split(",") })}
 				/>
 				<div className={classes.fileInput}>
