0cbcc482c076ae6120f4449e4cf333825b76ef6d
Islam Sayed <islam.sayed8@gmail.com>
2022-01-18 11:57:43 +0200

    add LIKE, DELETE cases

--- client/src/reducers/posts.reducer.js
+++ client/src/reducers/posts.reducer.js
@@ -5,7 +5,10 @@ const reducer =  (state = [], action) => {
         case 'CREATE':
             return [...state, action.payload];
         case 'UPDATE':
+        case 'LIKE':
             return (state.map(p => p._id === action.payload._id ? action.payload : p));
+        case 'DELETE':
+            return (state.filter(p => p._id !== action.payload));
         default:
             return state;
     }
