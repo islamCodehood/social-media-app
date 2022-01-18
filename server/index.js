345c7ae2ff268d1ad5f075a9b69b5b96cc95c7c2
Islam Sayed <islam.sayed8@gmail.com>
2022-01-18 16:24:11 +0200

    dotenv configuration

--- server/index.js
+++ server/index.js
@@ -1,28 +1,26 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import postRoutes from './routes/posts.routes.js'
+import postRoutes from "./routes/posts.routes.js";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
 
-app.use('/posts', postRoutes);
-
-const CONNECTION_URL = "mongodb+srv://admin:imageConsultant@cluster0.jzgpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 5555;
+app.use("/posts", postRoutes);
 
 mongoose
-	.connect(CONNECTION_URL, {
+	.connect(process.env.CONNECTION_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
-	.then(() => app.listen(PORT, () => console.log(`server running at port: ${PORT}`)))
+	.then(() => app.listen(process.env.PORT, () => console.log(`server running at port: ${process.env.PORT}`)))
 	.catch((err) => {
 		console.log(err.message);
 	});
-
