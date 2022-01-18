9e16ba5e989e9b68997c920315ff86126889e466
Islam Sayed <islam.sayed8@gmail.com>
2022-01-18 11:59:59 +0200

    create delete, patch routes

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { getPosts, createPost, updatePost } from "../controllers/posts.controllers.js";
+import { getPosts, createPost, updatePost, deletePost, likePost } from "../controllers/posts.controllers.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
 router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.patch("/:id/likePost", likePost);
 
 export default router;
