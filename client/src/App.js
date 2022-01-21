8196ac4b659f90204b3fb310a5313983b305758c
Islam Sayed <islam.sayed8@gmail.com>
2022-01-21 14:48:21 +0200

    import NavBar comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getPosts } from "./actions/posts.actions";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import memories from "./images/memories.png";
+import { Container, Grow, Grid } from "@material-ui/core";
 import Posts from "./components/posts/posts.component";
 import Form from "./components/form/form.component";
 import useStyles from "./styles";
-function App() {
-	const dispatch = useDispatch();
-	const classes = useStyles();
-	const [currentId, setCurrentId] = useState(null);
-	useEffect(() => {
-		dispatch(getPosts());
-	}, [currentId, dispatch]);
+import NavBar from "./components/navbar/navbar.component";
 
-	return (
-		<Container maxWidth="lg">
-			<AppBar className={classes.appBar} position="static" color="inherit">
-				<Typography className={classes.heading} variant="h2" align="center">
-					Memories
-				</Typography>
-				<img className={classes.image} src={memories} alt="memories" height="90" />
-			</AppBar>
-			<Grow in>
-				<Grid container className={classes.mainContainer} justifyContent="space-between" align-items="stretch" spacing={3}>
-					<Grid item xs={12} sm={12} md={7}>
-						<Posts setCurrentId={setCurrentId} />
-					</Grid>
-					<Grid item xs={12} sm={12} md={4}>
-						<Form currentId={currentId} setCurrentId={setCurrentId} />
-					</Grid>
-				</Grid>
-			</Grow>
-		</Container>
-	);
+function App() {
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const [currentId, setCurrentId] = useState(null);
+  
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  return (
+    <Container maxWidth="lg">
+      <NavBar />
+      <Grow in>
+        <Grid
+          container
+          className={classes.mainContainer}
+          justifyContent="space-between"
+          align-items="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={12} md={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={12} md={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Grow>
+    </Container>
+  );
 }
 
 export default App;
