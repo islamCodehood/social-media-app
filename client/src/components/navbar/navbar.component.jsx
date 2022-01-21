fc927b3fbccbfa6ffe598578a2ebf1396fecc101
Islam Sayed <islam.sayed8@gmail.com>
2022-01-21 14:47:55 +0200

    create comp

---
+++ client/src/components/navbar/navbar.component.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { AppBar, Typography, ToolBar, Button, Avatar } from "@material-ui/core";
+import useStyles from "./navbar.styles.js";
+import memories from "../../images/memories.png";
+
+const NavBar = () => {
+	const classes = useStyles();
+	const [user, serUser] = useState();
+	return (
+		<AppBar className={classes.appBar} position="static" color="inherit">
+			<div className={classes.brandContainer}>
+				<Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
+					Memories
+				</Typography>
+				<img className={classes.image} src={memories} alt="memories" height="90" />
+			</div>
+			<ToolBar className={classes.toolbar}>
+				{user ? (
+					<div className={classes.profile}>
+						<Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+						<Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+						<Button variant="contained" color="secondary" className={classes.logout}>Logout</Button>
+					</div>
+				) : (
+					<Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+				)}
+			</ToolBar>
+		</AppBar>
+	);
+};
+
+export default NavBar;
---
+++ client/src/components/navbar/navbar.styles.js
@@ -0,0 +1,18 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 5,
+    margin: "30px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heading: {
+    color: "#00bb77",
+  },
+  image: {
+    marginLeft: "15px",
+  },
+}));
