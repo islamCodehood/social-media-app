5c6296195b137f406a5ee533408e92da771272c6
Islam Sayed <islam.sayed8@gmail.com>
2022-01-22 13:30:22 +0200

    create input comp

---
+++ client/src/components/input/input.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core'
+import { Visibility, VisibilityOff } from '@material-ui/icons'
+
+const Input = ({ name, label, type, half, autoFocus, handleChange, handleShowPassword }) => {
+    return (
+    <Grid item xs={12} sm={half ? 6: 12}>
+        <TextField
+            name={name}
+            label={label}
+            type={type}
+            autoFocus={autoFocus}
+            onChange={handleChange}
+            required
+            fullWidth
+            variant="outlined"
+            InputProps={name === 'password' ? ({
+                endAdornment: (
+                    <InputAdornment position="end">
+                        <IconButton onClick={handleShowPassword}>
+                            {type === 'password' ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                    </InputAdornment>
+                )
+            }) : null   }
+        />
+    </Grid>
+    )
+}
+
+export default Input;
