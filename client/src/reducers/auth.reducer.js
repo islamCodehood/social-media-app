1f90e093fd20c53a99c565c404ef98cc9f51ba10
Islam Sayed <islam.sayed8@gmail.com>
2022-01-22 13:50:46 +0200

    create auth reducer

---
+++ client/src/reducers/auth.reducer.js
@@ -0,0 +1,16 @@
+import {
+    AUTH, LOGOUT
+} from "../constants/actionTypes"
+
+const reducer = (state = {authData: null}, action) => {
+    switch (action.type) {
+        case AUTH:
+            return action?.payload;
+        case LOGOUT:
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default reducer;
