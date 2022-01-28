39c7bfe1ebb760fb24f012c1a88979f3ba67d46f
Islam Sayed <islam.sayed8@gmail.com>
2022-01-28 18:27:11 +0200

    Revert "ؤقثشفث شعفا شؤفهخىس"
    This reverts commit 19ffa95c0ff30741e062aa7525f2e802b04f91c3.

--- client/src/actions/auth.actions.js
+++ client/src/actions/auth.actions.js
@@ -3,22 +3,18 @@ import {
   } from "../constants/actionTypes";
   import * as api from "../api";
 
-export const signin =  (formData) => async (dispatch) => {
+export const signin =  (formData, history) => async (dispatch) => {
     try {
         //sign in
-        const {data} = await api.signin(formData)
-        dispatch({ type: AUTH , payload: data });
-        
+        history.push('/')
     } catch(err) {
         console.log(err)
     }
 }
-export const signup = (formData) => async (dispatch) => {
+export const signup = (formData, history) => async (dispatch) => {
     try {
         //sign up
-        const {data} = await api.signup(formData)
-        dispatch({ type: AUTH , payload: data });
-        
+        history.push('/')
     } catch(err) {
         console.log(err)
     }
