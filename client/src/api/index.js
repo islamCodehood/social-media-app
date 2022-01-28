9e9d4bd33e66ddb1a5086d5d3f2112ef323d7f00
Islam Sayed <islam.sayed8@gmail.com>
2022-01-28 18:28:57 +0200

    create api post requests for the user

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -15,3 +15,6 @@ export const updatePost = (id, updatedPost) =>
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
 
 export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+
+export const signin = (formData) => axios.post('http://localhost:5000/user/signin', formData);
+export const signup = (formData) => axios.post('http://localhost:5000/user/signup', formData);
