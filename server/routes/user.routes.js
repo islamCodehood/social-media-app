3102671f359e47076137d0ba55e80aca2d74c269
Islam Sayed <islam.sayed8@gmail.com>
2022-01-28 13:23:38 +0200

    create user routes

---
+++ server/routes/user.routes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {
+    signin,
+    signup
+} from "../controllers/user.controllers"
+
+const router = express.Router();
+
+//refer to controllers for these functions
+router.post('/signin', signin);
+router.post('/signup', signup);
+
+export default router;
+
