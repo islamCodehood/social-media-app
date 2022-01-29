332eb2cc6d762f22e88203f1ca17b6b490d232a2
Islam Sayed <islam.sayed8@gmail.com>
2022-01-29 16:44:58 +0200

    add auth middleware to required routes

--- server/routes/posts.routes.js
+++ server/routes/posts.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import auth from '../middleware/auth.middleware.js'
 import {
   getPosts,
   createPost,
@@ -10,9 +11,9 @@ import {
 const router = express.Router();
 
 router.get("/", getPosts);
-router.post("/", createPost);
-router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost);
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
 
 export default router;
