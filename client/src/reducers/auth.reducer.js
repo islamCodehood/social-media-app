a0462e1f11f91a5c7868278bd4243a367efa8f39
Islam Sayed <islam.sayed8@gmail.com>
2022-01-30 10:46:46 +0200

    set localstorage and spread state while returning new one

--- client/src/reducers/auth.reducer.js
+++ client/src/reducers/auth.reducer.js
@@ -3,9 +3,11 @@ import { AUTH, LOGOUT } from "../constants/actionTypes";
 const reducer = (state = { authData: null }, action) => {
   switch (action.type) {
     case AUTH:
-      return action?.payload;
+      localStorage.setItem("profile", JSON.stringify({ ...action?.payload }));
+      return {...state, authData: action?.payload}
     case LOGOUT:
-      return { authData: null };
+      localStorage.clear();
+      return { ...state, authData: action?.payload };
     default:
       return state;
   }
