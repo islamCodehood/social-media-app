a0105783a5e2a9e17104383038a5d6499f816e5a
Islam Sayed <islam.sayed8@gmail.com>
2022-01-30 10:42:07 +0200

    create axios interceptors

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,6 +4,13 @@ import axios from "axios";
 //dev URL
 const API = axios.create({baseURL: 'http://localhost:5000'})
 
+//this will intercepts every request
+API.interceptors.request.use((req) => {
+  if(localStorage.getItem('profile')) {
+    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+  }
+  return req;
+})
 
 export const fetchPosts = () => API.get('/posts');
 
