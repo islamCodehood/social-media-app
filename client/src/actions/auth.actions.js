7659659bbcb74af2117a82183059be019dfc4c1c
Islam Sayed <islam.sayed8@gmail.com>
2022-01-30 10:40:49 +0200

    add react router dom navigate to nav to home

--- client/src/actions/auth.actions.js
+++ client/src/actions/auth.actions.js
@@ -3,22 +3,22 @@ import {
   } from "../constants/actionTypes";
   import * as api from "../api";
 
-export const signin =  (formData) => async (dispatch) => {
+export const signin =  (formData, navigate) => async (dispatch) => {
     try {
         //sign in
         const {data} = await api.signin(formData)
         dispatch({ type: AUTH , payload: data });
-        
+        navigate("/", { replace: true });
     } catch(err) {
         console.log(err)
     }
 }
-export const signup = (formData) => async (dispatch) => {
+export const signup = (formData, navigate) => async (dispatch) => {
     try {
         //sign up
         const {data} = await api.signup(formData)
         dispatch({ type: AUTH , payload: data });
-        
+        navigate("/", { replace: true });
     } catch(err) {
         console.log(err)
     }
