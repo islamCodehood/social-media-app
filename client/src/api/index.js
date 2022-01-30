9a42827bb96804d5698ce94502d8b1d8875c5e9f
Islam Sayed <islam.sayed8@gmail.com>
2022-01-30 10:41:32 +0200

    create instance of axios

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 //prod URL
-//const url = "https://social-app-is.herokuapp.com/posts";
-
+//const API = axios.create({baseURL: 'https://social-app-is.herokuapp.coAPI
 //dev URL
-const url = "http://localhost:5000/posts";
+const API = axios.create({baseURL: 'http://localhost:5000'})
 
-export const fetchPosts = () => axios.get(url);
 
-export const createPost = (newPost) => axios.post(url, newPost);
+export const fetchPosts = () => API.get('/posts');
+
+export const createPost = (newPost) => API.post('/posts', newPost);
 
 export const updatePost = (id, updatedPost) =>
-  axios.patch(`${url}/${id}`, updatedPost);
+  API.patch(`/posts/${id}`, updatedPost);
 
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
 
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
-export const signin = (formData) => axios.post('http://localhost:5000/user/signin', formData);
-export const signup = (formData) => axios.post('http://localhost:5000/user/signup', formData);
+export const signin = (formData) => API.post('/user/signin', formData);
+export const signup = (formData) => API.post('/user/signup', formData);
