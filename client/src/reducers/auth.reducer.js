6ccbab2880013169af91791c512961b71b84a4ad
Islam Sayed <islam.sayed8@gmail.com>
2022-02-03 00:50:57 +0200

    add timeStamp to localStorage object

--- client/src/reducers/auth.reducer.js
+++ client/src/reducers/auth.reducer.js
@@ -3,7 +3,7 @@ import { AUTH, LOGOUT } from "../constants/actionTypes";
 const reducer = (state = { authData: null }, action) => {
   switch (action.type) {
     case AUTH:
-      localStorage.setItem("profile", JSON.stringify({ ...action?.payload }));
+      localStorage.setItem("profile", JSON.stringify({ ...action?.payload, timeStamp: new Date().getTime() }));
       return { ...state, authData: action?.payload };
     case LOGOUT:
       localStorage.clear();
