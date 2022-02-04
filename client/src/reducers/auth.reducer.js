dda1199905efbccc55d77fe444b37e607698a900
Islam Sayed <islam.sayed8@gmail.com>
2022-02-04 18:01:41 +0200

    change the default state to be equal localStorage

--- client/src/reducers/auth.reducer.js
+++ client/src/reducers/auth.reducer.js
@@ -1,9 +1,9 @@
 import { AUTH, LOGOUT } from "../constants/actionTypes";
 
-const reducer = (state = { authData: null }, action) => {
+const reducer = (state = { authData:  JSON.parse(localStorage.getItem('profile'))}, action) => {
   switch (action.type) {
     case AUTH:
-      localStorage.setItem("profile", JSON.stringify({ ...action?.payload, timeStamp: new Date().getTime() }));
+      localStorage.setItem("profile", JSON.stringify({ ...action?.payload }));
       return { ...state, authData: action?.payload };
     case LOGOUT:
       localStorage.clear();
